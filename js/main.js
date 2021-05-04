7f7ca6034f085f49a7a7d5a814d6d16ce080cea5
Egor <argonium329@gmail.com>
2021-05-04 20:49:40 +0300

    Added base for lifelessons
    Added js function to generate a random life lesson using constant values

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
+const lessons = [
+    'YOU MUST DEFEAT SHENG LONG TO STAND A CHANCE.',
+    'THE FIRST THING TO KNOW, IS TO KNOW ONESELF.',
+    'YOU SHOULD BE STRONGER!',
+    'EVERY MOMENT GIVES US A CHANCE TO BECOME MORE THAN WHAT WE ARE.',
+    'NO FORGIVENESS FOR LIES!'
+];
 const srcStatic='./assets/images/avatar.png';
 const srcAnimated='./assets/images/avatar.gif';
 const dynAvatarClass='.avatar.avatar-dynamic';
 const avatarMoveTimeout=200;
 
+function getRandomLesson(){
+    let value = Math.round(Math.random()*(lessons.length-1));
+    return lessons[value];
+}
 
 $(document).ready(function(){
     let avatarElem=$(dynAvatarClass);
